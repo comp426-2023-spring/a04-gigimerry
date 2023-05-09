8d1b476d585c71e7dbf30422837f3539d56be7cf
gigimerry <69761091+gigimerry@users.noreply.github.com>
2023-05-09 14:45:42 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 const app = express();
 var arg = minimist(process.argv.slice(2));
 const PORT = arg.port || 5000;
+
+app.post('/app/rpsls/play', (req, res) => {
+	res.status(200).send(rpsls(req.body.shot));
+});
+
+app.get('/app/rps/play/:shot', (req, res) => {
+	res.status(200).send(rps(req.params.shot));
+});
+
+app.get('/app/rpsls/play/:shot', (req, res) => {
+	res.status(200).send(rpsls(req.params.shot));
+});
+
+app.get('*', (req, res) => {
+	res.status(400).send("404 NOT FOUND");
+});
+
+app.listen(port, () => {
+	console.log(`Server listening on port ${port}`);
+});
